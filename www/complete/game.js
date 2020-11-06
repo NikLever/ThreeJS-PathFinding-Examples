03a0d6ff8f5d9e03f5b915619f92ceaf4e0f5586
Nicholas Lever <nik@catalystpics.co.uk>
2020-11-06 09:37:21 +0000

    Small change to complete

--- www/complete/game.js
+++ www/complete/game.js
@@ -1,4 +1,4 @@
-import * as THREE from '../libs/three.module.js';
+›import * as THREE from '../libs/three.module.js';
 import { GLTFLoader } from '../libs/GLTFLoader.js';
 import { RGBELoader } from '../libs/RGBELoader.js';
 import { Player } from '../libs/Player.js';
